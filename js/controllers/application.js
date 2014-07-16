c9f7a9854281c1aa69df3afc61643545d86a575a
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2014-07-16 14:44:03 +0300

    Added moviesController and selectedMovieController

--- js/controllers/application.js
+++ js/controllers/application.js
@@ -1,1 +1,24 @@
 MovieTracker.ApplicationController = Ember.Controller.extend();
+
+
+MovieTracker.moviesController = Ember.ArrayController.create();
+MovieTracker.moviesController.set('content', [
+  MovieTracker.Movie.create({
+    title: 'Movie 1',
+    rating: 4
+  }),
+  MovieTracker.Movie.create({
+      title: 'Movie 2',
+      rating: 5
+  })
+]);
+
+MovieTracker.selectedMovieController = Ember.ObjectController.create({
+  selectedMovie: [],
+  select: function(item) {
+    this.set('selectedMovie', item);
+  },
+  toggleWatched: function() {
+    this.selectedMovie.toggleProperty('watched');
+  }
+});
